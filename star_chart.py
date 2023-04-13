07dea44a994b4009ea5a6a52057ddf4be03f5771
JMori7694 <41706211+JMori7694@users.noreply.github.com>
2023-04-12 20:22:51 -0400

    Add files via upload

---
+++ star_chart.py
@@ -0,0 +1,93 @@
+# datetime library
+from datetime import datetime
+from geopy import Nominatim
+from tzwhere import tzwhere
+from pytz import timezone, utc
+# matplotlib to help display our star map
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+from matplotlib.patches import Circle
+# skyfield for star data
+from skyfield.api import Star, load, wgs84
+from skyfield.data import hipparcos
+from skyfield.projections import build_stereographic_projection
+
+# Loading Earth and Star Data
+# skyfield library to load star data
+# de421 shows position of earth and sun in space
+eph = load('de421.bsp')
+# hipparcos dataset contains star location data
+with load.open(hipparcos.URL) as f:
+    stars = hipparcos.load_dataframe(f)
+
+# Setting up the Obversation Location
+location = 'Times Square, New York, NY'
+when = '2023-01-01 00:00'
+
+# geopy library for latitude and longitute
+locator = Nominatim(user_agent='myGeocoder', timeout=3)
+location = locator.geocode(location)
+lat, long = location.latitude, location.longitude
+
+# tzwhere and pytz libraries to get the timezone of our location
+# convert date string into datetime object
+dt = datetime.strptime(when, '%Y-%m-%d %H:%M')
+# define datetime and convert to utc based on our timezone
+timezone_str = tzwhere.tzwhere().tzNameAt(lat, long)
+local = timezone(timezone_str)
+# get UTC from local timezone and datetime
+local_dt = local.localize(dt, is_dst=None)
+utc_dt = local_dt.astimezone(utc)
+
+# skyfield library to define the location and time of our observation and
+# define the position of our observer
+# find location of earth and sun and set the observer position
+sun = eph['sun']
+earth = eph['earth']
+# define observation time from our UTC datetime
+ts = load.timescale()
+t = ts.from_datetime(utc_dt)
+# define an observer using the world geodetic system data
+observer = wgs84.latlon(latitude_degrees=lat, longitude_degrees=long).at(t)
+position = observer.from_altaz(alt_degrees=90, az_degrees=0)
+
+# Calculate Star Positions
+ra, dec, distance = observer.radec()
+center_object = Star(ra=ra, dec=dec)
+
+center = earth.at(t).observe(center_object)
+projection = build_stereographic_projection(center)
+field_of_view_degrees = 180.0
+
+star_positions = earth.at(t).observe(Star.from_dataframe(stars))
+stars['x'], stars['y'] = projection(star_positions)
+
+# Build The Star Chart
+chart_size = 10
+max_star_size = 100
+limiting_magnitude = 10
+bright_stars = (stars.magnitude <= limiting_magnitude)
+magnitude = stars['magnitude'][bright_stars]
+
+fig, ax = plt.subplots(figsize=(chart_size, chart_size))
+
+
+border = plt.Circle((0, 0), 1, color='#16161d', fill=True)
+ax.add_patch(border)
+
+marker_size = max_star_size * 10 ** (magnitude / -2.5)
+
+ax.scatter(stars['x'][bright_stars], stars['y'][bright_stars],
+           s=marker_size, color='white', marker='.', linewidths=0,
+           zorder=2)
+
+horizon = Circle((0, 0), radius=1, transform=ax.transData)
+for col in ax.collections:
+    col.set_clip_path(horizon)
+
+ax.set_aspect('equal')
+ax.set_xlim(-1, 1)
+ax.set_ylim(-1, 1)
+plt.axis('off')
+plt.show()
